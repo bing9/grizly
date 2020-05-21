e6050148ebf2af10eb68ce77b9280a90cb61ca4b
malinaa96 <52569986+malinaa96@users.noreply.github.com>
2020-05-21 15:42:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="grizly",
-    version="0.3.4",
+    version="0.3.5",
     author="Alessio Civitillo",
     description="Small package to build SQL with a Pandas api, generate workflows and more.",
     long_description=long_description,
