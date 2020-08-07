1e372b59a9bc7e12a6f6536415469c6ef2b0753a
michalz92 <michal.zawadzki@te.com>
2020-08-07 14:37:04 +0200

    rq workers are now using the grizly environment and docker_home

--- tests/rq-workers/settings.py
+++
@@ -1,9 +0,0 @@
-REDIS_HOST = "pytest_redis"
-REDIS_PORT = 6379
-
-# You can also specify the Redis DB to use
-# REDIS_DB = 3
-# REDIS_PASSWORD = 'very secret'
-
-# Queues to listen on
-QUEUES = ["checks_queue", "submit_queue"]
