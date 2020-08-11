8b6962e57b42a62c0cf98dc9e6d9178abef6bd65
michalz92 <michal.zawadzki@te.com>
2020-08-11 15:24:24 +0000

    update

--- grizly/scheduling/system_jobs/agent.py
+++ grizly/scheduling/system_jobs/agent.py
@@ -1,53 +1,27 @@
 import logging
-from datetime import datetime, timedelta, timezone
 
 from rq import Queue
-from grizly.scheduling.registry import Trigger, Registry
+from grizly.scheduling.registry import Registry
 
-logger = logging.getLogger("distributed.worker")
-registry = Registry(env="dev")  # registry = Registry()
-checks_queue = Queue("checks_queue", connection=registry.con)
-submit_queue = Queue("submit_queue", connection=registry.con)
-
-
-def submit_trigger(trigger):
-    logger.info(f"Loading {trigger.name} jobs...")
-    jobs = trigger.get_jobs()
-    logger.info(f"{trigger.name}'s jobs loaded successfully")
-    for job in jobs:
-        if job.status != "running":
-            if job.type == "regular":
-                queue = submit_queue
-            elif job.type == "listener":
-                queue = checks_queue
-            else:
-                continue
-            logger.info(f"Submitting {job.name} to {queue.name}...")
-            queue.enqueue(job.submit)
-            logger.info(f"Job {job.name} has been successfully submitted to {queue.name}")
-            trigger.last_run = datetime.now(timezone.utc)
-            trigger.is_triggered = False
-        else:
-            logger.debug(f"Job {job.name} is already running")
+logger = logging.getLogger(__name__)
+registry = Registry(redis_host="pytest_redis")
+submit_queue = Queue("submit", connection=registry.con)
 
 
 def run():
 
-    logger.info("Loading triggers...")
-    triggers = registry.get_triggers()
-    logger.info("Triggers loaded successfully")
+    try:
 
-    for trigger in triggers:
-        if trigger.type == "cron":
-            # trigger only cron jobs
-            next_run = trigger.next_run
-            now = datetime.now(timezone.utc) + timedelta(minutes=1)
-            logger.info(f"Checking whether {trigger.name} should run... next run: {next_run}")
-            if next_run < now:
-                trigger.is_triggered = True
-        # listeners are triggered inside submit, so here we only check & submit them
-        if trigger.is_triggered:
-            submit_trigger(trigger)
+        logger.info("Loading triggers...")
+        triggers = registry.get_triggers()
+        logger.info("Triggers loaded successfully")
+
+        for trigger in triggers:
+            if trigger.is_triggered:
+                for job in trigger.downstream_jobs:
+                    submit_queue.enqueue(job.submit)
+    except:
+        raise ValueError("Something went wrong.")
 
 
 if __name__ == "__main__":
