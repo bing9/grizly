f3adbf065133e0c790cdbf97b087178daf0ab0b4
michalz92 <michal.zawadzki@te.com>
2020-08-28 08:54:20 -0400

    final push of the merged images experiment

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 docker-compose build --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-# docker tag grizly_notebook:latest acoeteam/grizly_notebook
+# docker tag docker_grizly_notebook:latest acoeteam/grizly_notebook
 # docker push acoeteam/grizly_notebook
