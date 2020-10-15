f5a3a9d17257bf57a21dddc1c7b15282640be50a
michalz92 <michal.zawadzki@te.com>
2020-10-15 17:11:55 +0200

    - reordered code in logical order - added type hints - fixed `_validate_fields()`` to work with the new way SFDC types are represented (variable length fields include their length in parentheses, eg. 'email(40)') - improved logging and exception handling in `_cast_records()` - added more formats to `_cast()`

--- grizly/drivers/sfdc.py
+++ grizly/drivers/sfdc.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
+
 import datetime
 from logging import Logger
-from typing import List, Union
+from typing import List, Union, Dict, Any
 
 from ..sources.rdbms.sfdc import sfdb
 from ..types import SFDB
@@ -12,23 +14,13 @@ class SFDCDriver(SQLDriver):
     def __init__(self, source: SFDB = sfdb, table: str = None, logger: Logger = None):
         super().__init__(source=source, table=table, logger=logger)
 
-    def _cast_records(self, records):
-        casted = []
-        for record in records:
-            record_casted = []
-            for i, val in enumerate(record):
-                col_dtype = self.dtypes[i]
-                val_casted = self._cast(val, dtype=col_dtype)
-                record_casted.append(val_casted)
-            casted.append(tuple(record_casted))
-        return casted
-
-    def to_records(self):
+    def to_records(self) -> List[tuple]:
         self._validate_fields()
         query = self.get_sql()
-        response = self.source.con.query(query)
-        records_raw = response["records"]
-        records = self._sfdc_records_to_records(records_raw)
+        table = getattr(self.source.con.bulk, self.table)
+        response = table.query(query)
+        # records_raw = response["records"]  # this is for non-bulk API
+        records = self._sfdc_records_to_records(response)
         # records_casted = [
         #     tuple(self._cast(val, dtype=self.dtypes[i]) for i, val in enumerate(record))
         #     for record in records
@@ -40,19 +32,58 @@ class SFDCDriver(SQLDriver):
         """Check if requested fields are in SF table
         and if can be pulled (we can't pull compound fields)
         """
-        fields_and_types = dict(zip(self.fields, self.types))
-        compound_types = ("address", "location")
-        compound_fields = [
-            field for field in fields_and_types if fields_and_types[field] in compound_types
-        ]
+        compound_fields = self._get_compound_fields()
         if compound_fields:
             raise ValueError(
                 "Compound fields are unsupported. Please remove the following fields:"
                 f"{compound_fields}"
             )
 
-    def _cast(self, val, dtype="auto"):
-        """Fix columns with mixed dtypes"""
+    def _get_compound_fields(self) -> List[str]:
+        fields_and_types = dict(zip(self.fields, self.types))
+        fields_and_types_cleaned = {
+            field: _type.split("(")[0] for field, _type in fields_and_types.items()
+        }
+        compound_types = ("address", "location")
+        compound_fields = [
+            field
+            for field in fields_and_types_cleaned
+            if fields_and_types_cleaned[field] in compound_types
+        ]
+        return compound_fields
+
+    def _remove_compound_fields(self) -> SFDCDriver:
+        for field in self._get_compound_fields():
+            self.remove(field)
+        return self
+
+    @staticmethod
+    def _sfdc_records_to_records(sfdc_records: List[Dict[str, Any]]) -> List[tuple]:
+        """Convert weird SFDC response to records"""
+        records = []
+        for i in range(len(sfdc_records)):
+            sfdc_records[i].pop("attributes")
+            records.append(tuple(sfdc_records[i].values()))
+        return records
+
+    def _cast_records(self, records: List[tuple]) -> List[tuple]:
+        casted = []
+        for record in records:
+            record_casted = []
+            for i, val in enumerate(record):
+                col_dtype = self.dtypes[i]
+                try:
+                    val_casted = self._cast(val, dtype=col_dtype)
+                except (AssertionError, NotImplementedError):
+                    msg = f"Column {self.columns[i]} seems to be in an unsupported format"
+                    self.logger.exception(msg)
+                    raise
+                record_casted.append(val_casted)
+            casted.append(tuple(record_casted))
+        return casted
+
+    def _cast(self, val: Any, dtype: str) -> Any:
+        """Fix columns with mixed/serialized dtypes"""
 
         if not val:
             return None
@@ -62,44 +93,41 @@ class SFDCDriver(SQLDriver):
         dtype_str = str(dtype_mapped)
         if "string" in dtype_str:
             casted = str(val)
-        elif "float" in dtype_str:
+        elif "float" in dtype_str or "double" in dtype_str:
+            # self.logger.info(f"{val}, {dtype_str}")
             casted = float(val)
         elif "date32" in dtype_str and type(val) == str:
             casted = datetime.datetime.strptime(val, "%Y-%m-%d")
+        # TODO: put below logic in _cast_datetime()
         elif "timestamp" in dtype_str and type(val) == str:
             casted = datetime.datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f%z")
+        elif "timestamp" in dtype_str and type(val) == int:
+            # check if it's UTC
+            assert len(str(val)) == 13, "Unrecognized timestamp format"
+            tz_str = str(val)[-3:]
+            utc_tz_str = "000"
+            if tz_str == utc_tz_str:
+                casted = datetime.datetime.fromtimestamp(val / 1000)
+            else:
+                # should convert to UTC, but hopefully we don't have to bother
+                raise NotImplementedError("Casting non-UTC timestamps is not yet supported.")
         else:
             return val
         return casted
 
-    @staticmethod
-    def _sfdc_records_to_records(sfdc_records):
-        """Convert weird SFDC response to records"""
-        records = []
-        for i in range(len(sfdc_records)):
-            sfdc_records[i].pop("attributes")
-            records.append(tuple(sfdc_records[i].values()))
-        return records
-
-    def _validate_groupable(self, table: str, fields: List[str]):
-        fields_info = self.source.table(table).sf_table.describe()["fields"]
-        non_groupable = [field["name"] for field in fields_info if not field["groupable"]]
-        invalid_fields = [field for field in fields if field in non_groupable]
-        if invalid_fields:
-            raise ValueError(
-                f"Ungroupable fields found: {invalid_fields}. Please remove them from your query."
-            )
+    def describe(self):
+        return dict(zip(self.columns, self.types))
 
     def select(self):
         raise NotImplementedError("Subquerying is not possible in SOSQL")
 
-    def groupby(self, fields: Union[List[str], str] = None):
+    def groupby(self, fields: Union[List[str], str] = None) -> SFDCDriver:
         """Adds GROUP BY statement.
 
         Parameters
         ----------
         fields : list or string
-            List of fields or a field, if None then all fields are grouped. Fields must be groupable.
+            Field or list of fields, if None then all fields are grouped. Fields must be groupable.
 
         Examples
         --------
@@ -115,9 +143,6 @@ class SFDCDriver(SQLDriver):
         -------
         QFrame
         """
-        # assert (
-        #     "union" not in self.store["select"]
-        # ), "You can't group by inside union. Use select() method first."
         if isinstance(fields, str):
             fields = [fields]
         table = self.data["select"]["table"]
@@ -125,3 +150,12 @@ class SFDCDriver(SQLDriver):
         self._validate_groupable(table=table, fields=fields_to_validate)
         super().groupby(fields=fields)
         return self
+
+    def _validate_groupable(self, table: str, fields: List[str]):
+        fields_info = self.source.table(table).sf_table.describe()["fields"]
+        non_groupable = [field["name"] for field in fields_info if not field["groupable"]]
+        invalid_fields = [field for field in fields if field in non_groupable]
+        if invalid_fields:
+            raise ValueError(
+                f"Ungroupable fields found: {invalid_fields}. Please remove them from your query."
+            )
