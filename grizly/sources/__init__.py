38e8f69f52305791b9d5968aa020d671113437c2
michalz92 <michal.zawadzki@te.com>
2020-10-22 18:01:55 +0200

    cleaned up imports in inits

--- grizly/sources/__init__.py
+++ grizly/sources/__init__.py
@@ -1,6 +1,4 @@
 # from .rdbms.aurora import AuroraDB
-from .rdbms.denodo import Denodo
-from .rdbms.redshift import Redshift
 from .rdbms.sfdc import SFDB, sfdb
 from .filesystem.local import LocalFS
 from .filesystem.old_s3 import S3
