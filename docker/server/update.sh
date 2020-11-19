f3d033f7a5d7a2284e43b10cdce2fddb78a4e21e
michalz92 <michal.zawadzki@te.com>
2020-11-19 08:04:49 -0500

    change: now not using cache when building worker image

--- docker/server/update.sh
+++ docker/server/update.sh
@@ -4,7 +4,7 @@ cd ../
 docker-compose build --no-cache --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 docker tag docker_grizly_notebook:latest grizly_notebook:latest
 cd server/dask_worker
-docker-compose build
+docker-compose build --no-cache
 docker tag dask_worker:latest acoeteam/dask_worker
 cd ../
 docker-compose up --build --force-recreate --scale dask_worker=2 --scale rq-worker=5
