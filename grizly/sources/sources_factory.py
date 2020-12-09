d2d181ce40744031be2d5450bfd2f1ffebede062
michalz92 <michal.zawadzki@te.com>
2020-12-09 19:58:55 +0100

    add: RedshiftSpectrum

--- grizly/sources/sources_factory.py
+++ grizly/sources/sources_factory.py
@@ -1,16 +1,16 @@
-
 from ..config import config
 from .rdbms.aurora import AuroraPostgreSQL
 from .rdbms.denodo import Denodo
 from .rdbms.mariadb import MariaDB
-from .rdbms.redshift import Redshift
+from .rdbms.redshift import Redshift, RedshiftSpectrum
 from .rdbms.sfdc import SFDB
 from .rdbms.sqlite import SQLite
 from .rdbms.tableau import Tableau
 
 supported_dbs = ("redshift", "denodo", "sqlite", "mariadb", "aurora", "tableau", "sfdc")
 
-def Source(dsn: str, source_name: str = None, dialect: str = None,  **kwargs):
+
+def Source(dsn: str, source_name: str = None, dialect: str = None, **kwargs):
     source_name = source_name or kwargs.get("db")
     config_data = config.get_service("sources")
     if config_data.get(dsn) is None and None in [source_name, dialect]:
@@ -22,8 +22,10 @@ def Source(dsn: str, source_name: str = None, dialect: str = None,  **kwargs):
 
     if source_name == "aurora" and dialect == "postgresql":
         return AuroraPostgreSQL(dsn=dsn, **kwargs)
-    elif source_name == "redshift":
+    elif source_name == "redshift" and dialect == "postgresql":
         return Redshift(dsn=dsn, **kwargs)
+    elif source_name == "redshift" and dialect == "spectrum":
+        return RedshiftSpectrum(dsn=dsn, **kwargs)
     elif source_name == "denodo":
         return Denodo(dsn=dsn, **kwargs)
     elif source_name == "sqlite":
@@ -35,8 +37,9 @@ def Source(dsn: str, source_name: str = None, dialect: str = None,  **kwargs):
     elif source_name == "tableau":
         return Tableau(dsn=dsn, **kwargs)
 
-    raise NotImplementedError(f"Source {source_name} not supported yet. Supported sources: {supported_dbs}")
-
+    raise NotImplementedError(
+        f"Source {source_name} not supported yet. Supported sources: {supported_dbs}"
+    )
 
 
 def SQLDB(dsn: str, **kwargs):
